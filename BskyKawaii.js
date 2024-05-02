d6aede8cdc05fb77a7bddce49536384786c98b4b
VRZ/AstralArchivists <92945138+AstralVRZ@users.noreply.github.com>
2024-05-02 10:22:45 +0200

    Create BskyKawaii.js

---
+++ BskyKawaii.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Bsky kawii
+// @namespace    https://astralvrz.github.io/
+// @version      2024-05-02
+// @description  Auto Bluesky kawii mode 🪷🌸
+// @author       Astral
+// @match        https://bsky.app/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bsky.app
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    var URL = "https://bsky.app/?kawaii=true"
+    location.replace(URL)
+})();
