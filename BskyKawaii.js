77e6a2b96ba7643063bd9c7e550116df2d1e9192
VRZ/AstralArchivists <92945138+AstralVRZ@users.noreply.github.com>
2024-05-02 14:53:46 +0200

    Minor improvement

--- BskyKawaii.js
+++ BskyKawaii.js
@@ -4,12 +4,15 @@
 // @version      2024-05-02
 // @description  Auto Bluesky kawaii mode 🪷🌸
 // @author       Astral
-// @match        https://bsky.app/
+// @match        https://bsky.app/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bsky.app
 // @grant        none
 // ==/UserScript==
 
 (function() {
     var URL = "https://bsky.app/?kawaii=true"
-    location.replace(URL)
+    if (location.href == URL) {
+        new Audio('https://file.garden/ZfoWoAeKR2HIs_IK/America%20Ya%20D.mp3').play()
+    } 
+    else {location.replace(URL)}
 })();
