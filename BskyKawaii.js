d76e7f6df97fdae2fc69495d8f2ee892c2d51d88
VRZ/AstralArchivists <92945138+AstralVRZ@users.noreply.github.com>
2024-05-05 23:51:55 +0200

    Oops probably shouldt do that, now I cant middle click any posts anymore

--- BskyKawaii.js
+++ BskyKawaii.js
@@ -4,7 +4,7 @@
 // @version      2024-05-02
 // @description  Auto Bluesky kawaii mode 🪷🌸
 // @author       Astral
-// @match        https://bsky.app/*
+// @match        https://bsky.app/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bsky.app
 // @grant        none
 // ==/UserScript==
